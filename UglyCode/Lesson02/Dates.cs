4eb0021e091afe586eef30093445f03427864ef6
David Gardiner <david@gardiner.net.au>
2013-11-29 14:29:38 +1030

    Dates

---
+++ UglyCode/Lesson02/Dates.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UglyCode.Lesson02
+{
+    public class Dates
+    {
+        public static void Main()
+        {
+            DateTime d1 = new DateTime();
+
+            DateTime d2 = DateTime.Now;
+
+            DateTime d3 = DateTime.UtcNow;
+
+            DateTimeOffset d4 = DateTime.Now;
+
+            DateTimeOffset d5 = DateTime.UtcNow;
+
+            DateTimeOffset d6 = DateTimeOffset.Now;
+
+            DateTimeOffset d7 = DateTimeOffset.UtcNow;
+        } 
+    }
+}
